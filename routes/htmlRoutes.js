a735d18f8c600d4f93869b17ebeeda88f139f735
John Nielsen <johnhayesnielsen@gmail.com>
2020-07-07 10:10:31 -0600

    started setting up port

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const path = require('path');
+
+// return notes.html file
+    router.get('/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/notes.html'));
+    });
+// return index.html file
+    router.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/index.html'));
+    });
+
+    // If no matching route is found default to home
+    router.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/index.html'));
+    });
+
+    module.exports = router
---
+++ server.js
@@ -0,0 +1,24 @@
+
+const path = require('path');
+const express = require('express');
+const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+const PORT = process.env.PORT || 3002;
+ let noteCount = 1;
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+// static files
+app.use(express.static('public'));
+// create a controller for our note app so we can handle the routes and the rendering of
+// views then require the controllers (modules)
+app.use('/api', apiRoutes);
+
+app.use('/', htmlRoutes);
+
+// Start the server & listen for requests
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
