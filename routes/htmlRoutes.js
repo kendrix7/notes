fb039d0e6aa213c98220bbe9c68dec4680901754
John Nielsen <johnhayesnielsen@gmail.com>
2020-07-07 16:54:39 -0600

    trying to fix api bug

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const path = require('path');
-
 // return notes.html file
     router.get('/notes', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/notes.html'));
--- server.js
+++ server.js
@@ -1,24 +1,23 @@
-
-const path = require('path');
 const express = require('express');
+const { notes } = require('./db/db.json');
+const fs = require('fs');
+const path = require('path');
+const PORT = process.env.PORT || 3001;
 const app = express();
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
-const PORT = process.env.PORT || 3001;
- let noteCount = 1;
+
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
-// static files
-app.use(express.static('public'));
-// create a controller for our note app so we can handle the routes and the rendering of
-// views then require the controllers (modules)
+
 app.use('/api', apiRoutes);
 
 app.use('/', htmlRoutes);
 
-// Start the server & listen for requests
+app.use(express.static(path.join(__dirname, "public")));
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
